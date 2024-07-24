7f87beed07ff732323d6e505606e48abd8462877
Niclas Jansson <njansson@kth.se>
2024-07-24 14:11:11 +0900

    Update to new interface

--- src/source_terms/idw_source_term.f90
+++ src/source_terms/idw_source_term.f90
@@ -46,6 +46,11 @@ module idw_source_term
   use file, only : file_t
   use comm, only : MPI_REAL_PRECISION, NEKO_COMM
   use intersection_detector, only : intersect_detector_t
+  use mesh, only : mesh_t
+  use stack, only : stack_i4_t, stack_pt_t   
+  use global_interpolation, only : global_interpolation_t
+  use point, only : point_t
+  use math, only : NEKO_EPS
   use mpi_f08
   use logger
   implicit none
@@ -54,7 +59,15 @@ module idw_source_term
   !> Inverse distance weighting source term.
   type, public, extends(source_term_t) :: idw_source_term_t
      !> Smallest distance between between points and dofs     
-     real(kind=dp) :: ds_min
+     real(kind=dp), private :: ds_min
+     type(intersect_detector_t), private :: intersect
+     type(global_interpolation_t), private :: global_interp
+     type(stack_pt_t), private ::  lagrangian_points
+     real(kind=dp), private, allocatable :: xyz(:,:)
+     real(kind=rp), private, allocatable :: fu_ib(:)
+     real(kind=rp), private, allocatable :: fv_ib(:)
+     real(kind=rp), private, allocatable :: fw_ib(:)
+     real(kind=rp), private :: pwr_param
    contains
      !> The common constructor using a JSON object.
      procedure, pass(this) :: init => idw_source_term_init_from_json
@@ -80,9 +93,9 @@ contains
     character(len=:), allocatable :: object_type
     integer :: n_regions, i, j, k, e
     character(len=LOG_SIZE) :: log_buf
-    real(kind=dp) :: dx_min, dy_min, dz_min
+    real(kind=dp) :: dx_min, dy_min, dz_min, aabb_padding
     type(field_t), pointer :: u
-    type(intersect_detector_t) :: intersect
+    type(point_t), pointer :: pt(:)
 
     ! Mandatory fields for the general source term
     call json_get_or_default(json, "start_time", start_time, 0.0_rp)
@@ -92,6 +105,10 @@ contains
     call this%init_base(fields, coef, start_time, end_time)
 
     call neko_log%section('Inverse distance weighting')
+
+    call json_get_or_default(json, "power_parameter", this%pwr_param, 0.5_rp)
+    write(log_buf, '(A,f5.2)')  'IDW Power  : ', this%pwr_param
+    call neko_log%message(log_buf)
     
     ! Naive apporach to find the smallest distance between two dofs in the mesh
     u => neko_field_registry%get_field('u')
@@ -133,13 +150,14 @@ contains
     write(log_buf, '(A,ES13.6)') 'Minimum ds :',  this%ds_min
     call neko_log%message(log_buf)
 
-    call intersect%init(u%msh)
+    aabb_padding = 2 * this%ds_min
+    
+    call this%intersect%init(u%msh, aabb_padding)
+    call this%lagrangian_points%init()
     
     call json%get('objects', json_object_list)
     call json%info('objects', n_children=n_regions)
     call json%get_core(core)
-
-
 
     if (n_regions .lt. 10) then
        write(log_buf, '(A, I1)') 'Objects    : ', n_regions
@@ -169,24 +187,99 @@ contains
     end do
     call neko_log%end()
 
+
+    ! Report total number of lagrangian points generated, this might differ
+    ! from the STL sources due to refinement (not implemented yet!)   
+    if (this%lagrangian_points%size() .lt. 1e1) then
+           call neko_log%message('Type       : '// trim(object_type))
+       write(log_buf, '(A, I1)') 'Tot lagpts : ', this%lagrangian_points%size()
+    else if (this%lagrangian_points%size() .lt. 1e2) then
+       write(log_buf, '(A, I2)') 'Tot lagpts : ', this%lagrangian_points%size()
+    else if (this%lagrangian_points%size() .lt. 1e3) then
+       write(log_buf, '(A, I3)') 'Tot lagpts : ', this%lagrangian_points%size()
+    else if (this%lagrangian_points%size() .lt. 1e4) then
+       write(log_buf, '(A, I4)') 'Tot lagpts : ', this%lagrangian_points%size()
+    else if (this%lagrangian_points%size() .lt. 1e5) then
+       write(log_buf, '(A, I5)') 'Tot lagpts : ', this%lagrangian_points%size()
+    else if (this%lagrangian_points%size() .ge. 1e6) then
+       write(log_buf, '(A, I6)') 'Tot lagpts : ', this%lagrangian_points%size()
+    else if (this%lagrangian_points%size() .ge. 1e7) then
+       write(log_buf, '(A, I7)') 'Tot lagpts : ', this%lagrangian_points%size()
+    else if (this%lagrangian_points%size() .ge. 1e8) then
+       write(log_buf, '(A, I8)') 'Tot lagpts : ', this%lagrangian_points%size()
+    end if
+    call neko_log%message(log_buf)
+    
+    allocate(this%xyz(3, this%lagrangian_points%size()))
+    allocate(this%fu_ib(this%lagrangian_points%size()))
+    allocate(this%fv_ib(this%lagrangian_points%size()))
+    allocate(this%fw_ib(this%lagrangian_points%size()))
+
+    pt => this%lagrangian_points%array()
+    do i = 1, this%lagrangian_points%size()
+       this%xyz(1, i) = pt(i)%x(1)
+       this%xyz(2, i) = pt(i)%x(2)
+       this%xyz(3, i) = pt(i)%x(3)
+    end do
+    
+    call this%global_interp%init(coef%dof)
+
+    call this%global_interp%find_points_xyz(this%xyz, &
+         this%lagrangian_points%size())
+    
   end subroutine idw_source_term_init_from_json
 
   subroutine idw_source_term_free(this)
     class(idw_source_term_t), intent(inout) :: this
 
     call this%free_base()
+
+    call this%lagrangian_points%free()
+
+    if (allocated(this%xyz)) then
+       deallocate(this%xyz)
+    end if
+
+    if (allocated(this%fu_ib)) then
+       deallocate(this%fu_ib)
+    end if
+
+    if (allocated(this%fv_ib)) then
+       deallocate(this%fv_ib)
+    end if
+
+    if (allocated(this%fw_ib)) then
+       deallocate(this%fw_ib)
+    end if
+
   end subroutine idw_source_term_free
 
   subroutine idw_source_term_compute(this, t, tstep)
     class(idw_source_term_t), intent(inout) :: this
     real(kind=rp), intent(in) :: t
     integer, intent(in) :: tstep
-    type(field_t), pointer :: u, v, w
-    
+    type(field_t), pointer :: u, v, w, fu, fv, fw
+    integer :: n
+
+    n = this%fields%item_size(1)
+
     u => neko_field_registry%get_field('u')
     v => neko_field_registry%get_field('v')
     w => neko_field_registry%get_field('w')
 
+    fu => this%fields%get(1)
+    fv => this%fields%get(2)
+    fw => this%fields%get(3)
+    
+    associate(global_interp => this%global_interp, &
+         fu_ib => this%fu_ib, fv_ib => this%fv_ib, fw_ib => this%fw_ib)
+
+      call global_interp%evaluate(fu_ib, u%x)
+      call global_interp%evaluate(fv_ib, v%x)
+      call global_interp%evaluate(fw_ib, w%x)
+
+    end associate
+    
   end subroutine idw_source_term_compute
 
   subroutine idw_init_boundary_mesh(this, json)
@@ -196,7 +289,9 @@ contains
     type(tri_mesh_t) :: boundary_mesh
     character(len=:), allocatable :: mesh_file_name
     character(len=LOG_SIZE) :: log_buf
-    
+    integer :: i, el_idx
+    type(stack_i4_t) :: overlaps
+
     call json_get(json, 'name', mesh_file_name)
     mesh_file = file_t(mesh_file_name)
     call neko_log%message('Filename   : '// trim(mesh_file_name))
@@ -223,8 +318,45 @@ contains
     if (boundary_mesh%nelv .eq. 0) then
        call neko_error('No elements in the boundary mesh')
     end if
-  
+
+    call overlaps%init()
+    
+    do i = 1, boundary_mesh%mpts
+       call this%intersect%overlap(boundary_mesh%points(i), overlaps)
+
+       if (.not. overlaps%is_empty()) then
+          call this%lagrangian_points%push(boundary_mesh%points(i))
+       end if
+
+       do while (.not. overlaps%is_empty())
+          el_idx = overlaps%pop()
+       end do
+       call overlaps%clear()
+
+    end do
+    
+    call boundary_mesh%free()
     
   end subroutine idw_init_boundary_mesh
+
+!  !> Compute weight field
+!  subroutine idw_compute_weight
+  
+  !> Inverse distance weighting coefficient
+  !! @param r Radial distance to Lagrangian point.
+  !! @param rmax Radial distance for sphere of influence.
+  pure function inv_dist_weight(r, rmax, p) result(idw)    
+    real(kind=rp), intent(in) :: r
+    real(kind=rp), intent(in) :: rmax
+    real(kind=rp), intent(in) :: p
+    real(kind=rp) :: idw
+    
+    if(r .ge. rmax) then
+       idw = 0.0_rp
+    else
+       idw = ((rmax-r)/(rmax * r + NEKO_EPS))**p
+    end if
+    
+  end function inv_dist_weight
 
 end module idw_source_term
